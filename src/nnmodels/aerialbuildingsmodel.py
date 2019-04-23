a7d4f33f93d637c486b8cc19c002160c25565a96
Etienne Landuré <elandure@gmail.com>
2019-04-23 19:13:05 +0200

    Many changes in model hyperparameters
    UNet model with batch normalization, drop-out and decay for image segmentation.
    Changes:
    > Learning rate is now equal to 1e-3,
    > Loss function is now categorical_crossentropy,
    > Batch normalization momentum is now equal to 0.90 (default is 0.99).

--- src/nnmodels/aerialbuildingsmodel.py
+++ src/nnmodels/aerialbuildingsmodel.py
@@ -50,7 +50,7 @@ class AerialBuildingsModel(NNModel):
         # 1 -> a building
         self.__nClasses  = 1
         # Input data shape
-        self.input_shape = (336, 336, 3)
+        self.input_shape = (256, 256, 3)
         # File extensions for data to predict
         self.FILE_EXTENSIONS = [
             "tif",
@@ -122,8 +122,8 @@ class AerialBuildingsModel(NNModel):
                 yield np.array(x), np.array(y)
 
         # Create a generator for each step
-        train_generator = createGenerator(train_dir, 4)
-        val_generator   = createGenerator(val_dir,   4)
+        train_generator = createGenerator(train_dir, 4) # 12600 images
+        val_generator   = createGenerator(val_dir,   4) # 5400 images
 
         # Datas
         self.datas = { "train_generator": train_generator, "val_generator": val_generator }
@@ -134,14 +134,14 @@ class AerialBuildingsModel(NNModel):
         # 3x3 Convolution
         conv1  = Conv2D(16, (3, 3), padding='same', data_format='channels_last', name='conv1_1')(inputs)
         print("conv1:", conv1.shape)
-        bnor1  = BatchNormalization(name='bnor1_1')(conv1)
+        bnor1  = BatchNormalization(name='bnor1_1', momentum=0.9)(conv1)
         acti1  = Activation(tf.nn.relu, name='acti1_1')(bnor1)
         # Dropout of 0.2
         drop1  = Dropout(0.2, name='drop1_1')(acti1)
         # 3x3 Convolution
         conv1  = Conv2D(16, (3, 3), padding='same', data_format='channels_last', name='conv1_2')(drop1)
         print("conv1:", conv1.shape)
-        bnor1  = BatchNormalization(name='bnor1_2')(conv1)
+        bnor1  = BatchNormalization(name='bnor1_2', momentum=0.9)(conv1)
         acti1  = Activation(tf.nn.relu, name='acti1_2')(bnor1)
         # 2x2 Max Pooling
         pool1  = MaxPooling2D(pool_size=(2, 2), name='pool1_1')(acti1)
@@ -150,14 +150,14 @@ class AerialBuildingsModel(NNModel):
         # 3x3 Convolution
         conv2  = Conv2D(32, (3, 3), padding='same', data_format='channels_last', name='conv2_1')(pool1)
         print("conv2:", conv2.shape)
-        bnor2  = BatchNormalization(name='bnor2_1')(conv2)
+        bnor2  = BatchNormalization(name='bnor2_1', momentum=0.9)(conv2)
         acti2  = Activation(tf.nn.relu, name='acti2_1')(bnor2)
         # Dropout of 0.2
         drop2  = Dropout(0.2, name='drop2_1')(acti2)
         # 3x3 Convolution
         conv2  = Conv2D(32, (3, 3), padding='same', data_format='channels_last', name='conv2_2')(drop2)
         print("conv2:", conv2.shape)
-        bnor2  = BatchNormalization(name='bnor2_2')(conv2)
+        bnor2  = BatchNormalization(name='bnor2_2', momentum=0.9)(conv2)
         acti2  = Activation(tf.nn.relu, name='acti2_2')(bnor2)
         # 2x2 Max Pooling
         pool2  = MaxPooling2D(pool_size=(2, 2), name='pool2_1')(acti2)
@@ -166,65 +166,65 @@ class AerialBuildingsModel(NNModel):
         # 3x3 Convolution
         conv3  = Conv2D(64, (3, 3), padding='same', data_format='channels_last', name='conv3_1')(pool2)
         print("conv3:", conv3.shape)
-        bnor3  = BatchNormalization(name='bnor3_1')(conv3)
+        bnor3  = BatchNormalization(name='bnor3_1', momentum=0.9)(conv3)
         acti3  = Activation(tf.nn.relu, name='acti3_1')(bnor3)
         # Dropout of 0.2
         drop3  = Dropout(0.2, name='drop3_2')(acti3)
         # 3x3 Convolution
         conv3  = Conv2D(64, (3, 3), padding='same', data_format='channels_last', name='conv3_2')(drop3)
         print("conv3:", conv3.shape)
-        bnor3  = BatchNormalization(name='bnor3_2')(conv3)
+        bnor3  = BatchNormalization(name='bnor3_2', momentum=0.9)(conv3)
         acti3  = Activation(tf.nn.relu, name='acti3_2')(bnor3)
         # 2x2 Max Pooling
         pool3  = MaxPooling2D(pool_size=(2, 2), name='pool3_1')(acti3)
 
         # ----- Fourth Convolution - Max Pooling -----
         # 3x3 Convolution
-        conv4  = Conv2D(128, (3, 3), padding='same', data_format='channels_last', name='test_conv4_1')(pool3)
+        conv4  = Conv2D(128, (3, 3), padding='same', data_format='channels_last', name='conv4_1')(pool3)
         print("conv4:", conv4.shape)
-        bnor4  = BatchNormalization(name='test_bnor4_1')(conv4)
-        acti4  = Activation(tf.nn.relu, name='test_acti4_1')(bnor4)
+        bnor4  = BatchNormalization(name='bnor4_1', momentum=0.9)(conv4)
+        acti4  = Activation(tf.nn.relu, name='acti4_1')(bnor4)
         # Dropout of 0.2
-        drop4  = Dropout(0.2, name='test_drop4_2')(acti4)
+        drop4  = Dropout(0.2, name='drop4_2')(acti4)
         # 3x3 Convolution
-        conv4  = Conv2D(128, (3, 3), padding='same', data_format='channels_last', name='test_conv4_2')(drop4)
+        conv4  = Conv2D(128, (3, 3), padding='same', data_format='channels_last', name='conv4_2')(drop4)
         print("conv4:", conv4.shape)
-        bnor4  = BatchNormalization(name='test_bnor4_2')(conv4)
-        acti4  = Activation(tf.nn.relu, name='test_acti4_2')(bnor4)
+        bnor4  = BatchNormalization(name='bnor4_2', momentum=0.9)(conv4)
+        acti4  = Activation(tf.nn.relu, name='acti4_2')(bnor4)
         # 2x2 Max Pooling
-        pool4  = MaxPooling2D(pool_size=(2, 2), name='test_pool4_1')(acti4)
+        pool4  = MaxPooling2D(pool_size=(2, 2), name='pool4_1')(acti4)
 
         # ----- Fifth Convolution -----
         # 3x3 Convolution
         conv5  = Conv2D(256, (3, 3), padding='same', data_format='channels_last', name='conv5_1')(pool4)
         print("conv5:", conv5.shape)
-        bnor5  = BatchNormalization(name='bnor5_1')(conv5)
+        bnor5  = BatchNormalization(name='bnor5_1', momentum=0.9)(conv5)
         acti5  = Activation(tf.nn.relu, name='acti5_1')(bnor5)
         # Dropout of 0.25
         drop5  = Dropout(0.25, name='drop5_1')(acti5)
         # 3x3 Convolution
         conv5  = Conv2D(256, (3, 3), padding='same', data_format='channels_last', name='conv5_2')(drop5)
         print("conv5:", conv5.shape)
-        bnor5  = BatchNormalization(name='bnor5_2')(conv5)
+        bnor5  = BatchNormalization(name='bnor5_2', momentum=0.9)(conv5)
         acti5  = Activation(tf.nn.relu, name='acti5_2')(bnor5)
 
         # ----- Sixth Convolution -----
         # 2x2 Up Sampling
-        upsp6  = UpSampling2D(size = (2,2), name='test_upsp6_1')(acti5)
+        upsp6  = UpSampling2D(size = (2,2), name='upsp6_1')(acti5)
         # Concatenation
-        conc6  = Concatenate(axis=3, name='test_conc6_1')([upsp6, acti4])
+        conc6  = Concatenate(axis=3, name='conc6_1')([upsp6, acti4])
         # 3x3 Convolution
-        conv6  = Conv2D(128, (3, 3), padding='same', data_format='channels_last', name='test_conv6_1')(conc6)
+        conv6  = Conv2D(128, (3, 3), padding='same', data_format='channels_last', name='conv6_1')(conc6)
         print("conv6:", conv6.shape)
-        bnor6  = BatchNormalization(name='test_bnor6_1')(conv6)
-        acti6  = Activation(tf.nn.relu, name='test_acti6_1')(bnor6)
+        bnor6  = BatchNormalization(name='bnor6_1', momentum=0.9)(conv6)
+        acti6  = Activation(tf.nn.relu, name='acti6_1')(bnor6)
         # Dropout of 0.2
-        drop6  = Dropout(0.2, name='test_drop6_2')(acti6)
+        drop6  = Dropout(0.2, name='drop6_2')(acti6)
         # 3x3 Convolution
-        conv6  = Conv2D(128, (3, 3), padding='same', data_format='channels_last', name='test_conv6_2')(drop6)
+        conv6  = Conv2D(128, (3, 3), padding='same', data_format='channels_last', name='conv6_2')(drop6)
         print("conv6:", conv6.shape)
-        bnor6  = BatchNormalization(name='test_bnor6_2')(conv6)
-        acti6  = Activation(tf.nn.relu, name='test_acti6_2')(bnor6)
+        bnor6  = BatchNormalization(name='bnor6_2', momentum=0.9)(conv6)
+        acti6  = Activation(tf.nn.relu, name='acti6_2')(bnor6)
 
         # ----- Seventh Convolution - Up Sampling -----
         # 2x2 Up Sampling
@@ -234,14 +234,14 @@ class AerialBuildingsModel(NNModel):
         # 3x3 Convolution
         conv7  = Conv2D(64, (3, 3), padding='same', data_format='channels_last', name='conv7_1')(conc7)
         print("conv7:", conv7.shape)
-        bnor7  = BatchNormalization(name='bnor7_1')(conv7)
+        bnor7  = BatchNormalization(name='bnor7_1', momentum=0.9)(conv7)
         acti7  = Activation(tf.nn.relu, name='acti7_1')(bnor7)
         # Dropout of 0.2
         drop7  = Dropout(0.2, name='drop7_2')(acti7)
         # 3x3 Convolution
         conv7  = Conv2D(64, (3, 3), padding='same', data_format='channels_last', name='conv7_2')(drop7)
         print("conv7:", conv7.shape)
-        bnor7  = BatchNormalization(name='bnor7_2')(conv7)
+        bnor7  = BatchNormalization(name='bnor7_2', momentum=0.9)(conv7)
         acti7  = Activation(tf.nn.relu, name='acti7_2')(bnor7)
 
         # ----- Eighth Convolution - Up Sampling -----
@@ -252,14 +252,14 @@ class AerialBuildingsModel(NNModel):
         # 3x3 Convolution
         conv8  = Conv2D(32, (3, 3), padding='same', data_format='channels_last', name='conv8_1')(conc8)
         print("conv8:", conv8.shape)
-        bnor8  = BatchNormalization(name='bnor8_1')(conv8)
+        bnor8  = BatchNormalization(name='bnor8_1', momentum=0.9)(conv8)
         acti8  = Activation(tf.nn.relu, name='acti8_1')(bnor8)
         # Dropout of 0.2
         drop8  = Dropout(0.2, name='drop8_1')(acti8)
         # 3x3 Convolution
         conv8  = Conv2D(32, (3, 3), padding='same', data_format='channels_last', name='conv8_2')(drop8)
         print("conv8:", conv8.shape)
-        bnor8  = BatchNormalization(name='bnor8_2')(conv8)
+        bnor8  = BatchNormalization(name='bnor8_2', momentum=0.9)(conv8)
         acti8  = Activation(tf.nn.relu, name='acti8_2')(bnor8)
 
         # ----- Ninth Convolution - Up Sampling -----
@@ -270,27 +270,27 @@ class AerialBuildingsModel(NNModel):
         # 3x3 Convolution
         conv9  = Conv2D(16, (3, 3), padding='same', data_format='channels_last', name='conv9_1')(conc9)
         print("conv9:", conv9.shape)
-        bnor9  = BatchNormalization(name='bnor9_1')(conv9)
+        bnor9  = BatchNormalization(name='bnor9_1', momentum=0.9)(conv9)
         acti9  = Activation(tf.nn.relu, name='acti9_1')(bnor9)
         # Dropout of 0.2
         drop9  = Dropout(0.2, name='drop9_1')(acti9)
         ## 3x3 Convolution
         conv9  = Conv2D(16, (3, 3), padding='same', data_format='channels_last', name='conv9_2')(drop9)
         print("conv9:", conv9.shape)
-        bnor9  = BatchNormalization(name='bnor9_2')(conv9)
+        bnor9  = BatchNormalization(name='bnor9_2', momentum=0.9)(conv9)
         acti9  = Activation(tf.nn.relu, name='acti9_2')(bnor9)
 
         # ----- Tenth Convolution (outputs) -----
         # 3x3 Convolution
         conv10 = Conv2D(2, (3, 3), padding='same', data_format='channels_last', name='conv10_1')(acti9)
         print("conv10:", conv10.shape)
-        bnor10 = BatchNormalization(name='bnor10_1')(conv10)
+        bnor10 = BatchNormalization(name='bnor10_1', momentum=0.9)(conv10)
         acti10 = Activation(tf.nn.sigmoid, name='acti10_1')(bnor10)
         # 1x1 Convolution
         conv10 = Conv2D(self.__nClasses, (1, 1), padding='same', data_format='channels_last', name='conv10_2')(acti10)
         print("conv10:", conv10.shape)
-        bnor10 = BatchNormalization(name='bnor10_2')(conv10)
-        acti10 = Activation(tf.nn.sigmoid, name='acti10_2')(bnor10)
+        bnor10 = BatchNormalization(name='bnor10_2', momentum=0.9)(conv10)
+        acti10 = Activation(tf.nn.softmax, name='acti10_2')(bnor10)
 
         # Set a new model with the inputs and the outputs (tenth convolution)
         self.setModel(Model(inputs=inputs, outputs=acti10))
@@ -308,10 +308,10 @@ class AerialBuildingsModel(NNModel):
         # Number of epochs
         epochs = 10
         # Learning rate
-        learning_rate = 1e-4
+        learning_rate = 1e-3
         # Compiling the model with an optimizer and a loss function
-        self._model.compile(optimizer=RMSprop(lr=learning_rate, decay=learning_rate/epochs),
-                        loss=binary_crossentropy,
+        self._model.compile(optimizer=Adam(lr=learning_rate, decay=learning_rate/epochs),
+                        loss=categorical_crossentropy,
                         metrics=["accuracy"])
 
         # Fitting the model by using our train and validation data
